51c47e6c969271aa95a181b9f4b9ccb13fb795e7
mghazyfh <127060793+mghazyfh@users.noreply.github.com>
2023-04-17 06:10:48 +0700

    menambahkan API mahasiswa

---
+++ tpmodul9_1302210027/Controllers/Mahasiswa.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+using tpmodul9_1302210027;
+
+namespace tpmodul9_1302210027.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Mahasiswa : ControllerBase
+    {
+        public static List<Mhs> _data = new List<Mhs>
+        {
+            new Mhs ("Muhammad Ghazy Fawaz Hakim", "1302210027"),
+            new Mhs ("Muhammad Aulia Rifqi Hafid", "1302210094"),
+            new Mhs ("Yakup Asmaidy Atanggae", "1302210113"),
+            new Mhs ("Bintang Rayhan", "1302213002"),
+            new Mhs ("Ahmad Muflih Nawir", "1302213007"),
+        };
+        // GET: api/<Mahasiswa>
+        [HttpGet]
+        public IEnumerable<Mhs> Get()
+        {
+            return _data;
+        }
+
+        // GET api/<Mahasiswa>/5
+        [HttpGet("{id}")]
+        public Mhs Get(int id)
+        {
+            if (id < 0 || id >= _data.Count)
+            {
+                NotFound();
+            }
+            return _data[id];
+        }
+
+        // POST api/<Mhs>
+        [HttpPost]
+        public void Post([FromBody] Mhs value)
+        {
+            _data.Add(value);
+        }
+        // DELETE api/<Mhs>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id < 0 || id >= _data.Count)
+            {
+                NotFound();
+            }
+            _data.RemoveAt(id);
+        }
+    }
+}
---
+++ tpmodul9_1302210027/Mhs.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace tpmodul9_1302210027
+{
+    public class Mhs
+    {
+        public string Nama { get; private set; }
+        public string Nim { get; private set; }
+
+        public Mhs (string nama, string nim) { this.Nama = nama; this.Nim = nim; }
+    }
+}
